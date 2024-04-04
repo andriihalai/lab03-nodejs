f6720e46e980d169001d48ca66ef203a1a76055e
Halai Andrii <halaiandriy@gmail.com>
2024-04-04 17:30:45 +0300

    Add endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/greet', (req, res) => {
+  res.send('Greetings from Ukraine!');
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
